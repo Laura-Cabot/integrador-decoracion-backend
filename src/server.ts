885e603d57fefaf27181a3df1fc9cd5993fd5c2d
Laura Cabot <Laura-Cabot@users.noreply.github.com>
2025-07-26 20:28:45 -0300

    Usar process.env.PORT y endpoint /health para Render

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,8 @@ import cors from 'cors';
 import productosRouter from './routes/producto.routes';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+
 
 app.use(cors());
 app.use(express.json());
